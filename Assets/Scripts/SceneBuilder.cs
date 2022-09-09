235c4843c7e9ee9b6de2a2af3143904d6dcb1eeb
Keijiro Takahashi <keijiro@gmail.com>
2022-09-09 16:48:29 +0900

    Rendering

--- Assets/Scripts/SceneBuilder.cs
+++ Assets/Scripts/SceneBuilder.cs
@@ -14,10 +14,23 @@ struct SceneConfig
     public int PoleCount;
     public float BaseRange;
     public float NodeStride;
+
+    [Space]
+    public float Lifetime;
+    public float Rotation;
+    public float Twist;
+    public float PoleDelay;
+    public float NodeDelay;
+    [Range(0, 0.5f)]
+    public float FadeRange;
+
+    [Space, Range(0, 1)]
     public float EmissionRate;
     public Color EmissionColor1;
     public Color EmissionColor2;
     public float EmissionIntensity;
+
+    [Space]
     public uint Seed;
 
     #endregion
@@ -29,6 +42,12 @@ struct SceneConfig
         { PoleCount = 80,
           BaseRange = 1.25f,
           NodeStride = 0.1f,
+          Lifetime = 5,
+          Rotation = 2,
+          Twist = 2,
+          PoleDelay = 2,
+          NodeDelay = 2,
+          FadeRange = 0.3f,
           EmissionRate = 0.4f,
           EmissionColor1 = Color.red,
           EmissionColor2 = Color.blue,
@@ -65,9 +84,15 @@ static class SceneBuilder
         // Pole population
         for (var i = 0; i < cfg.PoleCount; i++)
         {
-            // Position / angle
+            // Time parameter
+            var t = time - hash.Float(0, cfg.PoleDelay, seed++);
+
+            // Base position
             var pos = math.float3(hash.InCircle(seed++) * cfg.BaseRange, 0);
+
+            // Base angle
             var angle = hash.Bool(seed++) ? 0 : math.PI / 4;
+            angle += t * cfg.Rotation / cfg.Lifetime;
 
             // Emission color (zero for no-emission)
             var emission = cfg.ChooseEmission(hash.Float(seed++));
@@ -75,17 +100,25 @@ static class SceneBuilder
             // Probability decay coefficient
             var decay = hash.Float(0.1f, 0.96f, seed++);
 
-            //
-            var t = math.frac(time / hash.Float(1, 3, seed++));
-            var s = math.min(math.smoothstep(0, 0.5f, t), 1 - math.smoothstep(0.5f, 1, t));
-
             // Pole extension loop
-            for (var prob = 1.0f; prob > 0.2f;)
+            var min_prob = 0.2f * math.pow(decay, hash.Float(2, 7, seed++));
+            for (var prob = 1.0f; prob > min_prob;)
             {
+                // Per-node time parameter
+                var t2 = (t - pos.z * cfg.NodeDelay) / cfg.Lifetime;
+
+                // Fade in / out parameter
+                var f_i = math.smoothstep(0, cfg.FadeRange, t2);
+                var f_o = math.smoothstep(1 - cfg.FadeRange, 1, t2);
+
+                // Per-node scale / angle
+                var scale = f_i - f_o;
+                var angle2 = angle + (f_i + f_o) * cfg.Twist;
+
                 for (var k = 0; k < 4; k++)
                 {
                     // Rotation matrix
-                    var rot = float2x2.Rotate(angle);
+                    var rot = float2x2.Rotate(angle2);
 
                     // Board: Displacement / position
                     var d1 = math.float2(cfg.NodeStride / 2, 0);
@@ -96,30 +129,31 @@ static class SceneBuilder
                     var p2 = pos + math.float3(math.mul(rot, d2), 0);
 
                     // Board model
-                    if (hash.Float(seed++) < prob)
+                    if (math.max(0.2f, hash.Float(seed++)) < prob)
                         buffer[count++] = new Modeler(position: p1,
-                                                      rotation: angle,
-                                                      scale: s,
+                                                      rotation: angle2,
+                                                      scale: scale,
                                                       color: Color.black,
                                                       shape: shapes.board);
 
                     // Pole model
-                    buffer[count++] = new Modeler(position: p2,
-                                                  rotation: angle,
-                                                  scale: s,
-                                                  color: Color.black,
-                                                  shape: shapes.pole);
+                    if (0.2f < prob)
+                        buffer[count++] = new Modeler(position: p2,
+                                                      rotation: angle2,
+                                                      scale: scale,
+                                                      color: Color.black,
+                                                      shape: shapes.pole);
 
                     // Emitter model
                     if (emission.a > 0)
                         buffer[count++] = new Modeler(position: pos,
                                                       rotation: 0,
-                                                      scale: s,
+                                                      scale: scale,
                                                       color: emission,
                                                       shape: shapes.pole);
 
                     // Rotation
-                    angle += math.PI / 2;
+                    angle2 += math.PI / 2;
                 }
 
                 // Stride
@@ -127,22 +161,6 @@ static class SceneBuilder
 
                 // Probability decay
                 prob *= hash.Float(decay, 1, seed++);
-            }
-
-            // Emitter extension
-            var ext = emission.a > 0 ? hash.Int(10, seed++) : 0;
-
-            for (var j = 0; j < ext; j++)
-            {
-                // Emitter model
-                buffer[count++] = new Modeler(position: pos,
-                                              rotation: 0,
-                                              scale: s,
-                                              color: emission,
-                                              shape: shapes.pole);
-
-                // Stride
-                pos.z += cfg.NodeStride;
             }
         }
 
